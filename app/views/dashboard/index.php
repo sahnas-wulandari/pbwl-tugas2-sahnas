937a9e425560dde20e5a741addc54b327f459c83
sahnas-wulandari <134993102+sahnas-wulandari@users.noreply.github.com>
2023-06-20 14:56:37 +0700

    Add files via upload

---
+++ app/views/dashboard/index.php
@@ -0,0 +1,3 @@
+<h2>Dashboard</h2>
+
+<div class="info">Selamat Datang di Halaman Dashboard <strong>Pembayaran Listrik</strong></div>
---
+++ app/views/index/index.php
@@ -0,0 +1,3 @@
+<h2>Index</h2>
+
+<div class="info">Selamat Datang Pada Halaman Dashboard, <strong>E-PLN</strong></div>
